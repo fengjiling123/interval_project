c43d4c7a76d8ac9af33d7b64067147632550735f
fengjiling <jiling.feng@kangcenet.com>
2019-01-03 14:23:46 +0800

    路由与侧栏实现

--- src/components/side_menu/index.jsx
+++ src/components/side_menu/index.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import history from '../../ultils/history';
+import { Menu, Icon, Button } from 'antd';
+import { withRouter } from 'react-router-dom';
+import menuData from '../../ultils/menu_data';
 import './index.scss';
 
 class SideMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
 
-    render () {
-        return <div className="side-menu-container">
-            <div onClick={() => { history.push('/taskList') }}>任务列表</div>
-            <div onClick={() => { history.push('/logList') }}>日志列表</div>
-        </div>
-    }
+	render () {
+		const selectedKeys = window.location.pathname === '/' ? ['/taskList'] : [window.location.pathname];
+		return <div className="side-menu-container">
+			<Menu
+				selectedKeys={selectedKeys}
+				theme='dark'
+				onSelect={({ item, key }) => { history.push(key); }}
+			>
+				{menuData.map(item => (
+					<Menu.Item key={item.path} >
+						<Icon type={item.icon} />
+						{item.name}
+					</Menu.Item>
+				))}
+			</Menu>
+		</div>
+	}
 
 }
-
 
 SideMenu.propTypes = {
+
 }
 
-
-export default SideMenu;
+export default withRouter(SideMenu);
---
+++ src/ultils/menu_data.js
@@ -0,0 +1,6 @@
+const menuData = [
+	{ name: '任务列表', path: '/taskList', icon: 'project' },
+	{ name: '日志列表', path: '/logList', icon: 'book' },
+];
+
+export default menuData;
