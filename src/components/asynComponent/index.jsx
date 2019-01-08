07b9fc72339ab42a15b9c4a2696acf127ff66a28
fengjiling <jiling.feng@kangcenet.com>
2019-01-08 14:54:37 +0800

    路由按需加载优化

--- src/components/asynComponent/index.jsx
+++ src/components/asynComponent/index.jsx
@@ -4,8 +4,8 @@ import './index.scss';
 
 const AsynLoadingComponent = ({ isLoading, error }) => {
   if (isLoading) {
-    return <div className='aysn-load-status'>加载中...</div>;
-    // return null;
+    // return <div className='aysn-load-status'>加载中...</div>;
+    return null;
   } else if (error) {
     return <div className='aysn-load-status'>页面加载失败</div>;
   } else {
